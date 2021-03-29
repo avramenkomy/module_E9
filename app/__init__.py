ffca0e793884ab9d50eaffdc5adae50babd4b210
avramenkomy <avramenkomy@yandex.ru>
2021-03-29 10:59:20 +0300

    Add CRUD

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy as sqla
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = sqla(app)
+db.create_all()
 
-from app import routes
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from app import app
-from flask import jsonify, make_response, render_template
+from app import app, db
+from flask import jsonify, make_response, render_template, request
 from datetime import datetime, timedelta
 from random import randint
+from .models import Forecast
 
 CITY = 'Amsterdam'
 CITIES = ['amsterdam', 'moscow']
@@ -60,3 +61,45 @@ def weather_your_city():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.route('/forecast', methods=['POST'])
+def forecast():
+    city = request.args.get('city')
+    date = request.args.get('date')
+    date_format = datetime.strptime(data, '%d-%m-%y')
+    forecast = Forecast(
+        city=city,
+        date=date,
+        temperature = get_weather_for_date(date),
+    )
+    db.session.add(forecast)
+    db.session.commit()
+    return jsonify({'id': forecast._id}), 201
+
+
+@app.route('/forecast/<_id>', methods=['GET', 'PATCH'])
+def forecast_for_id(_id):
+    if request.method == 'PATCH':
+        temperature = request.args.get('temperature')
+
+        forecast = Forecast.query.get_or_404(_id)
+        forecast.temperature = temperature
+        db.session.commit()
+
+    elif request.method == 'GET':
+        forecast = Forecast.query.get_or_404(_id)
+        return jsonify({
+            'id': forecast._id,
+            'city': forecast.city,
+            'temperature': forecast.temperature,
+            'date': forecast.date
+        })
+
+
+@app.route('/delete_forecast/<_id>', methods=['DELETE'])
+def delete_forecast(_id):
+    forecast = Forecast.query.get_or_404(_id)
+    db.session.delete(forecast)
+    db.session.commit()
+    return jsonify({'result': True})
